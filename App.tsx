81692c5eb88ae87a5dba5e0c9551a449a9f5f7bc
Cleber Severo <clebersevero1998@gmail.com>
2025-07-09 17:05:20 -0300

    fix: app bar style

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { NunitoSans_400Regular, NunitoSans_700Bold, useFonts } from "@expo-google-fonts/nunito-sans";
 import { Routes } from "@routes/index";
-import { StatusBar } from "react-native";
+import { StatusBar, Text } from "react-native";
 import theme from "src/theme";
 import { ThemeProvider } from "styled-components/native";
 
@@ -9,8 +9,8 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar barStyle={"light-content"} backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Routes /> : "Carregando..."}
+      <StatusBar barStyle={"dark-content"} backgroundColor="transparent" translucent />
+      {fontsLoaded ? <Routes /> : <Text>Carregando...</Text>}
     </ThemeProvider>
   );
 }
