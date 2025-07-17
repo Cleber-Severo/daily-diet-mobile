d7e487c0878cc5ae07267b0cdbf419123acc22c3
Cleber Severo <clebersevero1998@gmail.com>
2025-07-16 22:20:38 -0300

    feat: add action buttons

--- src/screens/MealDetails/index.tsx
+++ src/screens/MealDetails/index.tsx
@@ -1,7 +1,7 @@
 import { ContentWrapper } from "@components/ContentWrapper";
 import { useNavigation } from "@react-navigation/native";
 import { Text } from "react-native";
-import { Container, Description, DetailContainer, Header, Icon, IconWrapper, Name, TimeLabel, TimeText, Title } from "./styles";
+import { Actions, Container, ContentScroll, DeleteBtn, DeleteIcon, DeleteTitle, Description, DetailContainer, EditBtn, EditIcon, EditTitle, Header, Icon, IconWrapper, Name, TimeLabel, TimeText, Title } from "./styles";
 import { StatusLabel } from "./components/StatusLabel";
 
 export function MealDetails() {
@@ -15,16 +15,36 @@ export function MealDetails() {
         </IconWrapper>
         <Title>Refeição</Title>
       </Header>
+
       <ContentWrapper>
-        <DetailContainer>
-          <Name>X-tudo</Name>
-          <Description>Xis completo da lancheria do bairro</Description>
+        <ContentScroll
+          contentContainerStyle={{
+            paddingBottom: 32,
+            paddingTop: 24,
+          }}
+          showsVerticalScrollIndicator={false}
+        >
+          <DetailContainer>
+            <Name>X-tudo</Name>
+            <Description>Xis completo da lancheria do bairro</Description>
 
-          <TimeLabel>Data e hora</TimeLabel>
-          <TimeText>12/08/2022 às 20:00</TimeText>
+            <TimeLabel>Data e hora</TimeLabel>
+            <TimeText>12/08/2022 às 20:00</TimeText>
 
-          <StatusLabel type="SECONDARY" />
-        </DetailContainer>
+            <StatusLabel type="SECONDARY" />
+          </DetailContainer>
+        </ContentScroll>
+
+        <Actions>
+          <EditBtn>
+            <EditIcon />
+            <EditTitle>Editar refeição</EditTitle>
+          </EditBtn>
+          <DeleteBtn>
+            <DeleteIcon />
+            <DeleteTitle>Excluir refeição</DeleteTitle>
+          </DeleteBtn>
+        </Actions>
       </ContentWrapper>
     </Container>
   );
--- src/screens/MealDetails/styles.ts
+++ src/screens/MealDetails/styles.ts
@@ -1,33 +1,20 @@
-import { ButtonIconTypeStyleProps } from "@screens/home/components/StatisticsBtn/styles";
-import { ArrowLeftIcon } from "phosphor-react-native";
-import { View } from "react-native";
 import styled from "styled-components/native";
+import { Pressable, ScrollView, TouchableOpacity, View } from "react-native";
+import { ArrowLeftIcon, PencilSimpleLine, Trash } from "phosphor-react-native";
+import { ButtonIconTypeStyleProps } from "@screens/home/components/StatisticsBtn/styles";
 
 type ContainerProps = {
   type: ButtonIconTypeStyleProps;
 };
 
-export const Container = styled(View)<ContainerProps>`
-  background-color: ${({ theme, type }) => (type === "PRIMARY" ? theme.COLORS.green_light : theme.COLORS.red_light)};
-
+export const Container = styled.View<ContainerProps>`
   flex: 1;
+  background-color: ${({ theme, type }) => (type === "PRIMARY" ? theme.COLORS.green_light : theme.COLORS.red_light)};
   padding-top: 24px;
 `;
 
-export const IconWrapper = styled.TouchableOpacity`
-  position: absolute;
-  left: 24px;
-  top: 24px;
-  /* transform: translateY(-12px);  */
-`;
-
-type HeaderProps = {
-  type: ButtonIconTypeStyleProps;
-};
-
-export const Header = styled.View<HeaderProps>`
+export const Header = styled.View<ContainerProps>`
   background-color: ${({ theme, type }) => (type === "PRIMARY" ? theme.COLORS.green_light : theme.COLORS.red_light)};
-
   padding: 24px;
   align-items: center;
   justify-content: center;
@@ -36,13 +23,25 @@ export const Header = styled.View<HeaderProps>`
 
 export const Title = styled.Text`
   color: ${({ theme }) => theme.COLORS.gray_100};
-
   font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
   font-size: 18px;
 `;
 
+export const ContentWrapper = styled.View`
+  flex: 1;
+  justify-content: space-between;
+`;
+
+export const ContentScroll = styled(ScrollView)`
+  flex: 1;
+`;
+
 export const DetailContainer = styled.View`
   padding: 40px 24px;
+`;
+
+export const StatusLabelWrapper = styled.View`
+  margin-top: 24px;
 `;
 
 export const Name = styled.Text`
@@ -50,6 +49,7 @@ export const Name = styled.Text`
   font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
   font-size: 20px;
 `;
+
 export const Description = styled.Text`
   color: ${({ theme }) => theme.COLORS.gray_200};
   font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
@@ -63,11 +63,71 @@ export const TimeLabel = styled.Text`
   font-size: 14px;
   margin-top: 24px;
 `;
+
 export const TimeText = styled.Text`
   color: ${({ theme }) => theme.COLORS.gray_200};
   font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
   font-size: 16px;
   margin-top: 4px;
+`;
+
+export const Actions = styled.View`
+  gap: 10px;
+  padding: 24px;
+`;
+
+export const EditBtn = styled(TouchableOpacity)`
+  background-color: ${({ theme }) => theme.COLORS.gray_200};
+  border-radius: 6px;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 6px;
+
+  padding: 16px;
+`;
+
+export const EditTitle = styled.Text`
+  color: ${({ theme }) => theme.COLORS.white};
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  font-size: 14px;
+`;
+export const DeleteTitle = styled.Text`
+  color: ${({ theme }) => theme.COLORS.gray_200};
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  font-size: 14px;
+`;
+
+export const DeleteBtn = styled(TouchableOpacity)`
+  background-color: ${({ theme }) => theme.COLORS.white};
+  border: 1px solid ${({ theme }) => theme.COLORS.gray_200};
+  border-radius: 6px;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 6px;
+
+  padding: 16px;
+`;
+
+export const EditIcon = styled(PencilSimpleLine).attrs(({ theme }) => ({
+  size: 18,
+  color: theme.COLORS.white,
+  weight: "regular",
+}))``;
+
+export const DeleteIcon = styled(Trash).attrs(({ theme }) => ({
+  size: 18,
+  color: theme.COLORS.gray_200,
+  weight: "regular",
+}))``;
+
+export const IconWrapper = styled.TouchableOpacity`
+  position: absolute;
+  left: 24px;
+  top: 24px;
 `;
 
 export const Icon = styled(ArrowLeftIcon).attrs(({ theme }) => ({
